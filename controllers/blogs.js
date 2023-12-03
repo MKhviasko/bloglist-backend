f519a0af8ef4a67c23e6314ada7609fe1b9e3fe7
Monika Khviasko <monika.khviasko@sap.com>
2023-12-03 23:36:47 +0300

    feat: add CRUD endpoints to the blog controller

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,6 +8,31 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  const blogToDelete = await Blog.findById(request.params.id)
+  if (!blogToDelete) {
+    response.status(204).end()
+  }
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  try {
+    const body = request.body;
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, body, { new: true });
+
+    if (updatedBlog) {
+      response.status(200).json(updatedBlog);
+    } else {
+      response.status(404).json({ error: 'Blog not found' });
+    }
+  } catch (error) {
+    console.error('Error updating blog:', error);
+    response.status(500).json({ error: 'An error occurred while updating the blog' });
+  }
+})
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
